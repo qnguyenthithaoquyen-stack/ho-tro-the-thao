bb788df1f94367bbb8bf5e6414a08fabb6f23b6d
qnguyenthithaoquyen-stack <qnguyenthithaoquyen@gmail.com>
2025-08-28 10:33:48 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,12 @@
 // firebase-config.js
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+// **FIX:** Changed to export the firebaseConfig object
+export const firebaseConfig = {
   apiKey: "AIzaSyA2dXWCyjL2xgCSyeW9QQ7RvjI_O7kFHJw",
   authDomain: "sporthealthdata.firebaseapp.com",
   projectId: "sporthealthdata",
-  storageBucket: "sporthealthdata.firebasestorage.app",
+  storageBucket: "sporthealthdata.appspot.com",
   messagingSenderId: "789054240877",
   appId: "1:789054240877:web:04a400c9ea586523a86764",
   measurementId: "G-ZWS9C7P359"
 };
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
